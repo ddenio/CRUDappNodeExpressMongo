b9ebdec98010934949ac6ee0014365e28dd8dbe0
Derin DeNio <derindenio@gmail.com>
2023-12-02 19:56:13 -0700

    Third Commit

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ const express = require('express');
 //Turns out, Express doesn’t handle reading data from the <form> element on it’s own. We have to add another package called body-parser to gain this functionality.
 //Body-parser is a middleware. They help to tidy up the request object before we use them. Express lets us use middleware with the use method.
 const bodyParser = require('body-parser')
+//Once installed, we can connect to MongoDB through the MongoClient’s connect method as shown in the code below:
+const MongoClient = require('mongodb').MongoClient
+const uri = "mongodb+srv://ddenio:Kb35Xoac4MzNG4Gp@cluster0.ljynoar.mongodb.net/?retryWrites=true&w=majority";
 const app = express();
+
+
 
 //Make sure you place body-parser before your CRUD handlers!
 //The urlencoded method within body-parser tells body-parser to extract data from the <form> element and add them to the body property in the request object.
@@ -35,3 +40,9 @@ app.get('/', (req, res) => {
 app.post('/quotes', (req, res) => {
     console.log(req.body)
   })
+
+
+//   MongoClient.connect("mongodb+srv://ddenio:Kb35Xoac4MzNG4Gp@cluster0.ljynoar.mongodb.net/?retryWrites=true&w=majority", (err, client) => {
+//     if (err) return console.error(err)
+//   console.log('Connected to Database')
+//   })
